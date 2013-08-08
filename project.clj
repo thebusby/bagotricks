67ce86291cbda4810c9e970b18b69ad9765d4bd4
Alan Busby <thebusby@gmail.com>
2013-08-08 19:02:20 +0900

    Changed from CC0 license to MIT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bagotricks "1.5.2"
   :description  "A little bag of tricks for use with Clojure"
   :url          "http://github.com/thebusby/bagotricks"
-  :license      {:name "CC0"
-                 :url "http://creativecommons.org/publicdomain/zero/1.0/"}
+  :license      {:name "MIT"
+                 :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
